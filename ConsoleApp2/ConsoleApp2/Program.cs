5d670a9ace200c1659b6c65e8b113567570e0231
s28775 <s28775@pjwstk.edu.pl>
2024-03-19 02:00:15 +0100

    New feature MAX

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -13,3 +13,17 @@ static double Avg(int[] array)
     }
     return (double)sum/array.Length;
 }
+
+
+static int Max(int[] array)
+{
+    int max = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    return max;
+}
