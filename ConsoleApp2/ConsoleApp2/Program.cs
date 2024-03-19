a00f68219d7dade202440a0ac13399e91ee9de9c
s28775 <s28775@pjwstk.edu.pl>
2024-03-19 01:38:45 +0100

    Nowa funkcja AVG

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -2,3 +2,14 @@
 Console.WriteLine("Hellooooooooo 1");
 Console.WriteLine("Hellooooooooo 2");
 Console.WriteLine("Hellooooooooo 3");
+
+
+static double Avg(int[] array)
+{
+    int sum = 0;
+    foreach (int num in array)
+    {
+        sum += num;
+    }
+    return (double)sum/array.Length;
+}
